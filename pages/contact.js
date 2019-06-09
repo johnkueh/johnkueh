c720497ba594582036ea01f0c83058563eb26a5b
John Kueh <yoongfook@gmail.com>
2019-06-09 22:03:36 +1000

    Added airtable form

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import Link from 'next/link';
 import Layout from '../layouts/main';
 
 export default () => (
   <Layout>
-    <h1>Get in touch</h1>
-    <div>
-      <Link href="/">
-        <a>Go to Home</a>
-      </Link>
+    <div className="row d-flex justify-content-center">
+      <div className="col-md-8">
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" />
+        <iframe
+          title="Contact form"
+          className="airtable-embed airtable-dynamic-height"
+          src="https://airtable.com/embed/shrlvz9QWpffIOZ7v?backgroundColor=green"
+          frameBorder="0"
+          onMouseWheel=""
+          width="100%"
+          height="1044"
+          style={{ background: 'transparent' }}
+        />
+      </div>
     </div>
   </Layout>
 );
